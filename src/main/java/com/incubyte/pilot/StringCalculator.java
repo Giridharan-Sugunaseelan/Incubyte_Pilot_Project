f1f07431726b05c523d0e87094439a32c74f17d3
Giridharan <slbg.giridharan@gmail.com>
2024-06-09 08:24:33 +0530

    Initial commit

---
+++ src/main/java/com/incubyte/pilot/StringCalculator.java
@@ -0,0 +1,11 @@
+package com.incubyte.pilot;
+
+public class StringCalculator {
+
+    public int add(String input){
+        if(input.isEmpty()){
+            return 0;
+        }
+        return -1;
+    }
+}
